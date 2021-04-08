32f76a60a44dc8df6b4aec01c4d63622b60cff47
Kanchan Pandit <kanchan.pandit@gmail.com>
2021-04-08 20:40:02 +0530

    Initial-commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+	System.out.println("This is Maven Program");
     }
 
     private final String getMessage() {
